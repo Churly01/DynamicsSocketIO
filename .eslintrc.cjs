ecf353337cf9494cfdeb2d615686aa4f0f5db944
churly <nietopetinalcarlos@gmail.com>
2023-05-27 14:08:15 +0200

    Add changes

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    env: { browser: true, es2020: true, node:true
+	 },
+    extends: [
+	'eslint:recommended',
+    ],
+    
+    parserOptions: { ecmaVersion: 'latest', sourceType: 'module'
+                   },
+}
--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const {Server} = require('socket.io');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = new Server(server,{
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  }
+});
 
 io.on('connection', (socket) => {
   console.log('New client connected');
-
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
-
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     io.emit('chat message', msg);
